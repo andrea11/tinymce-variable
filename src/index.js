9f31bf1853a08f6f34d891adf7ff3c4d94d59ac4
Andrea ACCARDO <Andrea.ACCARDO@amadeus.com>
2017-07-11 11:12:36 +0200

    add index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Exports the "variable" plugin for usage with module loaders
+// Usage:
+//   CommonJS:
+//     require('tinymce/plugins/variable')
+//   ES2015:
+//     import 'tinymce/plugins/variable'
+require('./plugin.js');
